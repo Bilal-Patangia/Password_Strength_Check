9db1433a7ed89a52b029a42119053d3dc2248c3e
Bilal-Patangia <m.bilalpatangia9@gmail.com>
2026-01-01 23:11:12 +0530

    Add files via upload

---
+++ password_strength.py
@@ -0,0 +1,25 @@
+def check_password_strength(password):
+    strength = 0
+
+    if len(password) >= 8:
+        strength += 1
+    if any(char.isupper() for char in password):
+        strength += 1
+    if any(char.islower() for char in password):
+        strength += 1
+    if any(char.isdigit() for char in password):
+        strength += 1
+    if any(char in "!@#$%^&*()-_+=<>?" for char in password):
+        strength += 1
+
+    if strength <= 2:
+        return "Weak Password"
+    elif strength == 3 or strength == 4:
+        return "Moderate Password"
+    else:
+        return "Strong Password"
+
+
+password = input("Enter your password: ")
+result = check_password_strength(password)
+print("Password Strength:", result)
